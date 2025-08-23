99df7355f91edcc345a91dfef16f77f32ca1b867
Bahri Munawwar <munawwarbahri@gmail.com>
2025-08-23 17:13:05 +0700

    feat: add models, migrations, and seeders for categories, items, orders, and roles

---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Item;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Item::factory(10)->create();
+        // This will create 10 items with random data using the ItemFactory.
+    }
+}
