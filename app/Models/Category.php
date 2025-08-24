928f9681c7eca0c562289c2a5aa9348e8c14af31
Bahri Munawwar <munawwarbahri@gmail.com>
2025-08-25 00:08:35 +0700

    feat: implement soft deletes and relationships in models for Category, Item, Order, OrderItem, Role, and User

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use softDeletes;
+
+    protected $fillable = ['cat_name', 'description', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function items()
+    {
+        return $this->hasMany(item::class);
+    }   
 }
